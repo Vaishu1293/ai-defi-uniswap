e5bc9dbf419beff890558eaf447a4ac352eb2f4a
vaish shivkumar <vaish.1231993.vaishali@gmail.com>
2025-07-06 17:56:51 +0100

    home-page-alignment

---
+++ src/components/home/HomeMain.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ThemeChanger from "./ThemeChanger";
+import TradeMain from "../trade/TradeMain";
+
+const HomeMain = () => {
+  
+  return (
+    <>
+      <ThemeChanger />
+      <TradeMain/>
+    </>
+  );
+};
+
+export default HomeMain;
